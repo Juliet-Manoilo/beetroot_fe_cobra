7582d46d7cc5cfd4ade5a2afa9c5c16e0c5731da
juliett manoilo <mnl.juliett@gmail.com>
2020-11-10 02:05:34 +0200

    :constructions: sections added

---
+++ projects/new/src/js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const listOfItem = document.querySelectorAll('.shopping-list__label');
+    const ul
+})
+
+
+    
+
+    listOfItem.forEach(element => {
+        element.addEventListener('click', (e) => {
+            const innerText = e.target.parentElement.innerText.trim();
+            const innerHtml = e.target.parentElement;
+            if (generalParenElement.classList.contains('shopping-list__label')) {
+                if 
+            }
+        })
+    })
